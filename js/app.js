bbe4ffae59132ae86f57c4eee8fe5e5441f3da3a
anniewu <annie@hyweb.com.tw>
2023-03-17 10:39:48 +0800

    20230317新增跑馬燈

--- js/app.js
+++ js/app.js
@@ -248,4 +248,20 @@ $(function() {
             $(' .ask_questions .questions_block').slideUp();
         }
     });
+    // 跑馬燈
+    if ($('.marquee').length > 0) {
+        $('.marquee ul').slick({
+            dots: false,
+            infinite: true,
+            vertical: true,
+            verticalSwiping: true,
+            speed: 300,
+            autoplaySpeed: 5000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            pauseOnHover: true, //滑鼠移過後暫停自動撥放
+            focusOnSelect: true,
+        });
+    }
 })
