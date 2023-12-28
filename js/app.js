58b8d27b5ebfabe3636b01ff7a9c315a3dd8201a
anniewuu00 <annie@hyweb.com.tw>
2023-12-28 11:44:58 +0800

    20231228修改js

--- js/app.js
+++ js/app.js
@@ -88,6 +88,7 @@ $(function () {
   /*-----------------------------------*/
   /////////////fatfooter開關/////////////
   /*-----------------------------------*/
+  $('.btn-fatfooter').attr('title', '收合選單/CLOSE');
   $('.btn-fatfooter').click(function (e) {
     $(this)
       .parent('#FatFooter')
@@ -97,9 +98,11 @@ $(function () {
         if ($(this).is(':visible')) {
           $('.btn-fatfooter').html('收合/CLOSE');
           $('.btn-fatfooter').attr('name', '收合選單/CLOSE');
+          $('.btn-fatfooter').attr('title', '收合選單/CLOSE');
         } else {
           $('.btn-fatfooter').html('展開/OPEN');
           $('.btn-fatfooter').attr('name', '展開選單/OPEN');
+          $('.btn-fatfooter').attr('title', '展開選單/OPEN');
         }
       });
     $(this).stop(true, true).toggleClass('close');
